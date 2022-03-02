09fc673da98051c11eee520789409f04de395169
Alexis Creuzot <alexis@monoqle.fr>
2022-03-02 08:11:05 -0600

    analytics

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -3,11 +3,20 @@
     // Analytics
 
     ackeeTracker.create('https://ackee-qsnz.onrender.com').record('5512170c-57cd-4cc4-bb59-b704ac436c89')
-    ackeeTracker.action('2f6c2595-a8df-4383-a3ba-799b53fb7dd1', { key: 'Click', value: 1 })
-    ackeeTracker.action('c9d11d68-06a1-490b-b08b-cfb7cb0978c0', { key: 'Click', value: 1 })
-    ackeeTracker.action('f5cba383-de08-4811-b55e-e790e8a38dc3', { key: 'Click', value: 1 })
-    ackeeTracker.action('dfea01c5-a6b5-44c7-abaa-c3579e144598', { key: 'Click', value: 1 })
-
+    
+    $("a#subscribe").click(function(){
+        ackeeTracker.action('2f6c2595-a8df-4383-a3ba-799b53fb7dd1', { key: 'Click', value: 1 })
+    });
+    $("a#academic").click(function(){
+        ackeeTracker.action('c9d11d68-06a1-490b-b08b-cfb7cb0978c0', { key: 'Click', value: 1 })
+    });
+    $("a#converse").click(function(){
+        ackeeTracker.action('f5cba383-de08-4811-b55e-e790e8a38dc3', { key: 'Click', value: 1 })
+    });
+    $("a#apply").click(function(){
+        ackeeTracker.action('dfea01c5-a6b5-44c7-abaa-c3579e144598', { key: 'Click', value: 1 })
+    });
+    
     // UI
 
     var quotes = $(".slide");
