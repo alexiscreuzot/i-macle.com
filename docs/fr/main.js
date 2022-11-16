d5d89c0e645e614fc7e4c3844b336bc50a2ddf64
Alexis Creuzot <alexis@monoqle.fr>
2022-11-15 22:07:45 -0600

    update

--- docs/fr/main.js
+++ docs/fr/main.js
@@ -1,1 +1,35 @@
-!function(){function e(){++a,l.eq(a%l.length).fadeIn(1e3).delay(2e3).fadeOut(1e3,e)}var l=$(".slide"),a=-1;e(),$(".abstract").click(function(){$(this).toggleClass("show")})}(),ScrollReveal({reset:!0}),ScrollReveal().reveal(".show-once",{reset:!1}),ScrollReveal().reveal(".fade-in",{duration:2e3,move:0}),ScrollReveal().reveal(".scale-up",{duration:2e3,scale:.9});
+(function() {
+    // UI
+
+    var quotes = $(".slide");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(1000)
+            .delay(2000)
+            .fadeOut(1000, showNextQuote);
+    }
+    showNextQuote();
+
+    $(".abstract").click(function() {
+        $(this).toggleClass("show");
+    });
+
+})();
+
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".show-once", {
+  reset: false });
+
+
+ScrollReveal().reveal(".fade-in", {
+  duration: 2000,
+  move: 0 });
+
+
+ScrollReveal().reveal(".scale-up", {
+  duration: 2000,
+  scale: 0.9 });
